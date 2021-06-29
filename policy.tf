c05cba735c25d26894d772b232566c724dfe2549
alisaazi2 <alisaazimova88@gmail.com>
2021-06-29 01:15:56 +0000

    added policy

---
+++ policy.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_policy" "policy" {
+  name = "admins"
+  path = "/"
+  description = "admins"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy_attachment" "test-attach" {
+  group      = aws_iam_group.developers.name
+  policy_arn = aws_iam_policy.policy.arn
+}
